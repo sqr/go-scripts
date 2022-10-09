6c1d03cd6e092602825e609c6366ac329690b8f3
Arturo Bracero <artubr@gmail.com>
2022-10-09 15:19:25 +0200

    init

---
+++ dyndns/main.go
@@ -0,0 +1,6 @@
+// Set credentials
+// Get number of domains to sync from command line execution. This way we don't have to edit the script when adding new subdomains
+// Set input in new array
+// Get current IP address using http://checkip.amazonaws.com/
+// On each element, check current value using AWS API
+// If the value has changed, update on AWS
